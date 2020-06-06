67513e4ab3781cfd2cc616f8d2275bbb64afca2f
Antonio Zhu <antoniozhu@hotmail.com>
2020-06-07 00:11:20 +0200

    Implemented load balancing, query injection and playlist rewriting

--- server.py
+++ server.py
@@ -10,10 +10,11 @@ import json
 
 import re 
 
-from urllib.parse import urlparse
+from urllib.parse import urlparse, parse_qs, urlunparse, urlencode
 from contextlib import closing
 
-hostname = 'en.wikipedia.org'
+playlists : dict = None 
+load_balancing = {}
 
 base_url_regex = '^http.?:\/\/.+.\..+?(\/.+)'
 
@@ -23,13 +24,6 @@ def merge_two_dicts(x, y):
     z.update(y)    # modifies z with y's keys and values & returns None
     return z
 
-def set_header():  
-    # Now this is where I inject
-    headers = {
-        'Host': hostname
-    }
-
-    return headers
 
 class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
     protocol_version = 'HTTP/1.0'
@@ -39,35 +33,80 @@ class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self, body=True):
         sent = False
         try:
-
-            url = 'http://{}{}'.format(hostname, self.path)
             req_header : dict = self.parse_headers()
 
             # print(req_header)
             # print(url)
 
-            query = urlparse(self.path).query
+            parse = urlparse(self.path)
 
-            if ( '/playlist/' in self.path ): 
+            query = parse_qs( parse.query  )
+             
+            if ( 'get_playlist' in query.keys() ): 
+                
                 # Rewrite URLs
                 print('Rewriting playlist')
+
+                # Select URL by playlist query
+                provider = list(filter( lambda x : x['name'] == query['get_playlist'][0] , playlists))[0]
+
+                url = provider['playlist_url']
+
+                resp = requests.get(url, headers=merge_two_dicts(req_header, {"Host" : urlparse(url).netloc } ), params=provider['users'][0]['params'] , verify=False)
+                
+                content = resp.content.decode().split('\n')
+                
+                for i in range(0, len(content)):
+                    line : str = content[i]
+                    match : re.Match = re.match(base_url_regex, line)
+                    if match != None :
+                        line_parsed = urlparse(line)
+
+                        line_query = parse_qs(line_parsed.query)
+                        line_query['playlist'] = query['get_playlist'][0]
+                        # TODO semicolon must be replaced in a better way
+                        built_string = urlunparse( ('http', req_header['Host'], line_parsed.path.replace("\r", ""), urlencode(query=line_query) , None , None  ) )
+                        content[i] = built_string.replace(";", "?")
+
                 self.send_response(200)
-                self.wfile.write( 'Placeholder'.encode(encoding='UTF-8') )
+                self.send_resp_headers(resp)
+                self.wfile.write( '\n'.join(content).encode() )
                 sent = True 
                 return
-                 
+           
+            if ( 'playlist' in query.keys() ):
+                provider = list(filter( lambda x : x['name'] == query['playlist'][0] , playlists))[0]
 
+                url = provider['stream_host']
 
-            resp = requests.get(url, headers=merge_two_dicts(req_header, set_header()), verify=False, stream=True)
-            
-            self.send_response(resp.status_code)
-            self.send_resp_headers(resp)
-            if body:
-                for chunk in resp.iter_content(4096):
-                    self.wfile.write(chunk)
-            sent = True    
-            return
+                # TODO: Restream
 
+                # TODO: Make threshold configurable
+                user_list =  list(filter(lambda x : "used" not in x.keys() or x["used"] < 1, provider['users']))
+
+                if ( len(user_list) == 0 ): 
+                    self.send_error(404, "Out of connections! \n")
+                    return
+
+                user = user_list[0]
+                user["used"] = 1
+
+                resp = requests.get(url + self.path , headers=merge_two_dicts(req_header, {"Host" : urlparse(url).netloc } ), params=user['params'] , verify=False,  stream=True)
+
+                self.send_response(resp.status_code)
+                self.send_resp_headers(resp)
+                if body:
+                    for chunk in resp.iter_content(4096):
+                        self.wfile.write(chunk)
+                sent = True    
+                return
+
+            else:
+                self.wfile.write('Use query params \n'.encode())
+                self.send_response(200)
+
+        except Exception as e:
+            print(e)
         finally:
             # self.finish()
             if not sent:
@@ -124,8 +163,12 @@ def parse_args(argv=sys.argv[1:]):
 
 def main(argv=sys.argv[1:]):
     args = parse_args(argv)
+
+    global playlists 
+    playlists = json.load( open('playlists.json', 'r') )
+
     print('http server is starting on port {}...'.format(args.port))
-    server_address = ('127.0.0.1', args.port)
+    server_address = ('0.0.0.0', args.port)
     httpd = HTTPServer(server_address, ProxyHTTPRequestHandler)
     print('http server is running as reverse proxy')
     httpd.serve_forever()
